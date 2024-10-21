e4f70a607646d3fcb577cba731369a2c77194173
Gasanova Tamara <solas93@mail.ru>
2024-10-21 21:05:10 +0300

    feat: implement test-class tests(8-10)

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -48,13 +48,32 @@ describe('BankAccount', () => {
 
   test('fetchBalance should return number in case if request did not failed', async () => {
     // Write your tests here
+    const account = getBankAccount(30);
+
+    await account.fetchBalance().then((res) => {
+      if(res) {
+        expect(res).toBeTruthy();
+      }
+    })
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
     // Write your tests here
+    const account = getBankAccount(20);
+    await account.fetchBalance().then((res) => {
+      if (res) {
+        expect(account.synchronizeBalance()).resolves.toBe(res);
+      }
+    })
   });
 
   test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
     // Write your tests here
+    const account = getBankAccount(20);
+    await account.fetchBalance().then((res) => {
+      if (!res) {
+        expect(account.synchronizeBalance()).resolves.toThrow('Synchronization failed');
+      }
+    })
   });
 });
